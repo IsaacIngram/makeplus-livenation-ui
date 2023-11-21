7ac567af75c29caf086db9de4d28975fc51c3b3c
IsaacIngram <isaac@isaacingram.com>
2023-11-20 20:04:59 -0600

    Initial commit

---
+++ control_api/light_control.py
@@ -0,0 +1,33 @@
+###############################################################################
+#
+# File: control_lights.py
+#
+# Author: Isaac Ingram
+#
+# Purpose:
+# Provide API for skylight control.
+# 
+###############################################################################
+
+def set_diffuse(id: int, value: int):
+    """
+    Set the diffuse value of a skylight.
+
+    Params:
+    id: The id of the skylight to modify
+    value: Value for diffuse
+    """
+    #TODO: implement
+    print(f"Set diffuse of fixture {id} to {value}")
+    
+
+def set_blackout(id: int, value: int):
+    """
+    Set the blackout value of a skylight.
+    
+    Params:
+    id: The id of the skylight to modify
+    value: Value for blackout
+    """
+    #TODO: implement
+    print(f"Set blackout of fixture {id} to {value}")
